f7b68e9d11dd6ff340e9dad41fd6d9adfb745348
daniellop31 <daniellop31@yahoo.es>
2020-02-23 21:24:29 -0500

    CREACION DE CATEGORIA E INSTRUMENTO

--- src/routes/routesCategorias.js
+++ src/routes/routesCategorias.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/nuevaCategoria', (req, res) => {
-    res.render('nuevaCategoria');
+const Categoria = require('../models/categoria');
+
+router.get('/nuevaCategoria', async (req, res) => {
+    const categorias = await Categoria.find();
+    res.render('nuevaCategoria', {categorias});
+});
+
+router.post('/guardarCategoria', async (req, res)=>{
+    const nombre = req.body;
+    var continuar = true;
+    if(nombre == ''){
+        continuar = false;
+    }
+    if(continuar){
+        const categoria = new Categoria(req.body);
+        await categoria.save();
+        const categorias = await Categoria.find();
+        res.render('nuevaCategoria', {categorias});
+    }else{
+        const categorias = await Categoria.find();
+        console.log('Faltan Datos En El Formulario');
+        res.render('nuevaCategoria', {categorias});
+    }
+    //res.send('Categoria insertada');
 });
 
 module.exports = router;
--- src/routes/routesInstrumentos.js
+++ src/routes/routesInstrumentos.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
+//MODELO PRINCIPAL DE DATOS
 const Instrumento = require('../models/instrumento');
 
-router.get('/nuevoInstrumento', (req, res) => {
-    res.render('nuevoInstrumento');
+//Modelos auxiliares
+const Categoria = require('../models/categoria');
+
+router.get('/nuevoInstrumento', async (req, res) => {
+    const categorias = await Categoria.find();
+    var continuar = true;
+    res.render('nuevoInstrumento', {categorias, continuar});
 });
 
 router.post('/guardarInstrumento', async (req, res) => {
-    const instrumento = new Instrumento(req.body);
-    await instrumento.save();
-    res.send('Se le tiene');
+    const {nombre, descripcion, categoria, objetivos, proposito, t_Duracion, n_Dificultad} = req.body;
+    var continuar = true;
+    if(nombre == ''){
+        continuar = false;
+    }
+    if(descripcion.value == ''){
+        continuar = false;
+    }
+    if(categoria.value == ''){
+        continuar = false;
+    }
+    if(objetivos.value == ''){
+        continuar = false;
+    }
+    if(proposito.value == ''){
+        continuar = false;
+    }
+    if(t_Duracion.value == ''){
+        continuar = false;
+    }
+    if(n_Dificultad.value == ''){
+        continuar = false;
+    }
+    if(continuar){
+        const instrumento = new Instrumento(req.body);
+        await instrumento.save();
+        const categorias = await Categoria.find();
+        res.render('nuevoInstrumento', {categorias});
+    }else{
+        const categorias = await Categoria.find();
+        console.log('Faltan Datos En El Formulario');
+        res.render('nuevoInstrumento', {categorias});        
+    }
 });
 
 module.exports = router;
